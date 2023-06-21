ef81231c81fe107072668f3318d2634533b46f08
NathanBasol <basol.nathan@gmail.com>
2023-06-21 12:16:47 +0200

    classe connexionBdd et liste compteur dans RecjercheTrajetB

--- 2 - Java/2 - Code/Final/src/backend/RechercheTrajetB.java
+++ 2 - Java/2 - Code/Final/src/backend/RechercheTrajetB.java
@@ -3,8 +3,15 @@ package backend;
 import frontend.Menu;
 import javafx.application.Application;
 import javafx.stage.Stage;
+import utils.ConnexionBdd;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
 
 public class RechercheTrajetB extends Application {
+
+    private ArrayList<String> lesCompteurs;
 
     public static void main(String[] args) {
         launch(args);
@@ -17,4 +24,27 @@ public class RechercheTrajetB extends Application {
         menu.show();
         primaryStage.hide();
     }
+
+    public void lesCompteursBdd() {
+        this.lesCompteurs = new ArrayList<String>();
+        ConnexionBdd connexionBdd = new ConnexionBdd();
+
+        try {
+            String query = "SELECT DISTINCT nomCompteur FROM Compteur ORDER BY nomCompteur";
+            Statement statement = connexionBdd.getConnection().createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            while(resultSet.next()) {
+                this.lesCompteurs.add(resultSet.getString("nomCompteur"));
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<String> getLesCompteurs() {
+        return this.lesCompteurs;
+    }
 }
+
