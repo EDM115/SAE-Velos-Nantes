18b6d518d535bf07c8db06392685af0015cc3b4f
EDM115 <dev@edm115.eu.org>
2023-06-22 00:28:23 +0200

    del data.json on window exit

---
+++ 2 - Java/2 - Code/Final/src/backend/ResultatsRechercheB.java
@@ -0,0 +1,19 @@
+package backend;
+
+import frontend.Menu;
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class ResultatsRechercheB extends Application {
+	public static void main(String[] args) {
+        launch(args);
+    }
+
+	@Override
+    public void start(Stage primaryStage) {
+        // Create menu
+        Menu menu = new Menu(primaryStage);
+        menu.show();
+        primaryStage.hide();
+    }
+}
