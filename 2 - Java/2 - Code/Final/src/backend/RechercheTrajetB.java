5383252f6c3b2404be07852b2accadefde1c2f5a
NathanBasol <basol.nathan@gmail.com>
2023-06-21 23:02:47 +0200

    nathan7

--- 2 - Java/2 - Code/Final/src/backend/RechercheTrajetB.java
+++ 2 - Java/2 - Code/Final/src/backend/RechercheTrajetB.java
@@ -171,15 +171,8 @@ public class RechercheTrajetB extends Application {
             resultatsRecherche.start(this.rechercheTrajet.getStage());
 
             
-<<<<<<< HEAD
         } catch (SQLException e) {
             e.printStackTrace();
-        }
-=======
-        } catch (SQLSyntaxErrorException e) {
-            System.err.println("\u001B[31mERREUR\u001B[0m");
-        } catch (SQLException e) {
-            System.err.println("\u001B[31mREQUETE IMPOSSIBLE\u001B[0m");
         }
     }
 
@@ -223,7 +216,6 @@ public class RechercheTrajetB extends Application {
         }
 
         return file;
->>>>>>> 52fbed88d5a17b0571aafd636b913b993247453c
     }
 }
 
